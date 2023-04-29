2f76f42a0ed75f2f4a7a4d85f681a19d1390fe88
DarthRL <darthrlu@gmail.com>
2023-04-30 01:06:50 +0800

    2.19 failed

--- part2/countries/src/conponents/CountryList.js
+++ part2/countries/src/conponents/CountryList.js
@@ -2,20 +2,11 @@ import axios from 'axios'
 import {useState} from 'react'
 
 const CountryList = ({ filtered, handleShow }) => {
-  const[weather, setWeather] = useState(null)
   if (filtered.length === 1) {
     const country = filtered[0]
     const capital = country.capital
     const languages = country.languages
     const flag = country.flags.png
-    const weatherBaseUrl = "https://api.openweathermap.org/data/2.5/weather?q="
-    const picBaseUrl = "https://openweathermap.org/img/wn/"
-    const picEndUrl = "@2x.png"
-    const api_key = process.env.REACT_APP_API_KEY
-    axios.get(`${weatherBaseUrl}${capital}&units=metric&appid=${api_key}`).then(response => response.data)
-      .then(response => setWeather(response))
-    console.log(weather)
-    const icon = `${picBaseUrl}${weather.weather[0].icon}${picEndUrl}`
     return (
       <div>
         <h1>{country.name.common}</h1>
@@ -25,10 +16,6 @@ const CountryList = ({ filtered, handleShow }) => {
           {Object.keys(languages).map(key => <li key={key} >{languages[key]}</li>)}
         </ul>
         <img alt="flag" src={flag} />
-        <h2>Weather in {capital}</h2>
-        <p>temperature {weather.main.temp} Celsius</p>
-        <img alt={weather.weather[0].description} src={icon}/>
-        <p>wind {weather.wind.speed}m/s</p>
       </div>
     )
   }
