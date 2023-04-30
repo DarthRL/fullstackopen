8e4cbb9bbb6af886edb655c91908b6ef61822d9e
DarthRL <darthrlu@gmail.com>
2023-04-30 16:44:19 +0800

    2.20

--- part2/countries/src/conponents/CountryList.js
+++ part2/countries/src/conponents/CountryList.js
@@ -1,5 +1,25 @@
 import axios from 'axios'
-import {useState} from 'react'
+import { useState } from 'react'
+
+const Weather = ({ location }) => {
+  const [weather, setWeather] = useState(null)
+  const weatherBaseUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+  const picBaseUrl = "https://openweathermap.org/img/wn/"
+  const picEndUrl = "@2x.png"
+  const api_key = process.env.REACT_APP_API_KEY
+  axios.get(`${weatherBaseUrl}${location}&units=metric&appid=${api_key}`).then(response => response.data)
+    .then(response => setWeather(response))
+  if (!weather) return null
+  const icon = `${picBaseUrl}${weather.weather[0].icon}${picEndUrl}`
+  return (
+    <div>
+      <h2>Weather in {location}</h2>
+      <p>temperature {weather.main.temp} Celsius</p>
+      <img alt={weather.weather[0].description} src={icon} />
+      <p>wind {weather.wind.speed}m/s</p>
+    </div>
+  )
+}
 
 const CountryList = ({ filtered, handleShow }) => {
   if (filtered.length === 1) {
@@ -16,6 +36,7 @@ const CountryList = ({ filtered, handleShow }) => {
           {Object.keys(languages).map(key => <li key={key} >{languages[key]}</li>)}
         </ul>
         <img alt="flag" src={flag} />
+      <Weather location={capital} />
       </div>
     )
   }
